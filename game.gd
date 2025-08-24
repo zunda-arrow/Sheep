899f0b345168943d95deeee2b6c79de054bb94b0
Zunda <mailme@jacobhaussman.online>
2025-08-24 12:57:35 -0400

    preview sheep

--- game.gd
+++ game.gd
@@ -19,26 +19,33 @@ func _process(delta: float) -> void:
 		var pos = $MouseClicked.move($TileMap, mouse_pos)
 		$MouseClicked.position = $TileMap.map_to_local(pos)
 
-		var goto = truck_go_to($TileMap.map_to_local(mouse_pos), $TileMap.map_to_local(sheep.pos))
-		sheep.preview($TileMap.local_to_map(goto))
+		var info = truck_go_to($TileMap.map_to_local(mouse_pos), $TileMap.map_to_local(sheep.pos))
+		sheep.preview($TileMap.local_to_map(info[0]), info[1])
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.is_pressed():
+		mouse_pos = $TileMap.local_to_map($TileMap.get_local_mouse_position())
 		for sheep in sheepen:
-			sheep.go($TileMap.local_to_map(truck_go_to(get_local_mouse_position(), $TileMap.map_to_local(sheep.pos))))
+			var info = truck_go_to($TileMap.map_to_local(mouse_pos), $TileMap.map_to_local(sheep.pos))
+			sheep.go($TileMap.local_to_map(info[0]), info[1])
 
-func truck_go_to(mouse_pos: Vector2, truck_pos: Vector2) -> Vector2:
-	var angle_to_truck = mouse_pos.angle_to(truck_pos)
+func truck_go_to(mouse_pos: Vector2, truck_pos: Vector2) -> Array:
+	var angle_to_truck = (truck_pos - mouse_pos).angle()
 
 	var new_vec = (truck_pos - mouse_pos).normalized()
 	
 	var d = sqrt(mouse_pos.distance_squared_to(truck_pos))
-	var dist = d
+	
+	var max_dist = 300;
+	if d > max_dist:
+		d = max_dist
+	
+	var dist = max_dist - d
 	
 	var x = new_vec.x * dist
 	var y = new_vec.y * dist
 
-	return Vector2(
+	return [Vector2(
 		truck_pos.x + x,
 		truck_pos.y + y,
-	)
+	), angle_to_truck]
--- sheep.gd
+++ sheep.gd
@@ -8,12 +8,16 @@ func _ready() -> void:
 	position = $"../TileMap".map_to_local(pos)
 	$SheepGoTo.visible = true
 
-func go(to: Vector2i):
+func go(to: Vector2i, angle: float):
 	pos = to
 	position = $"../TileMap".map_to_local(to)
 
-func preview(to: Vector2):
+func preview(to: Vector2, angle: float):
 	$"SheepGoTo".global_position = $"../TileMap".map_to_local(to)
+	$SheepGoTo.rotation = rotate_sheep(angle)
+
+func rotate_sheep(facing: float):
+	return facing - (PI / 6)
 
 func move(tilemap: TileMap, to: Vector2):
 	pos = to
